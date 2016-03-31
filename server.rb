998683be9489bb7209d2960d99fa0c7e45821e85
Justin Barber <barber.justin@gmail.com>
2016-03-30 21:15:09 -0400

    add logging

--- server.rb
+++ server.rb
@@ -1,14 +1,21 @@
 require 'socket'
+require 'remote_syslog_logger'
+
+$logger = RemoteSyslogLogger.new('logs4.papertrailapp.com', 54460)
+$logger.formatter = proc do |severity, datetime, progname, msg|
+  "#{severity} #{msg}"
+end
 
 begin
   server = TCPServer.open(2000)
   loop do
     Thread.start(server.accept) do |client|
       while line = client.gets
-        puts line.chop
+        $logger.info(line.chop)
       end
       client.close
     end
   end
 rescue Interrupt
+  $logger.info("Goodbye!")
 end
